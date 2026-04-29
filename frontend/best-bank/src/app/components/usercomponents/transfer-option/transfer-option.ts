391238dcec019904649e69100f6f10ab354dc581
nicolaszbq <nicolascontatotrabalho@gmail.com>
2026-04-29 09:53:53 -0300

    Fixed the router problem on the money option component

--- frontend/best-bank/src/app/components/usercomponents/transfer-option/transfer-option.ts
+++ frontend/best-bank/src/app/components/usercomponents/transfer-option/transfer-option.ts
@@ -1,18 +1,23 @@
 import { Component, inject, Input } from '@angular/core';
 import { SharedModuleModule } from '../../../modules/shared-module/shared-module-module';
+import { Route, Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-transfer-option',
-  
+  standalone: true,
   imports: [
+    RouterModule,
     SharedModuleModule
   ],
   templateUrl: './transfer-option.html',
   styleUrl: './transfer-option.scss',
 })
 export class TransferOption {
-  
-  private router = inject(SharedModuleModule);
   @Input() name: string = "";
   @Input() page: string = "";
+
+  constructor(private router:Router){}
+  navigateToPage(){
+    this.router.navigate([this.page]);
+  }
 }
